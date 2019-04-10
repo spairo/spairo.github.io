7900fa908dcf3b94a462d55ca2901e955c985c1b
Luis Alberto Sanchez Rubio <luis.sanchez@globant.com>
2019-04-10 16:12:06 -0500

    device evals

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 console.log("loaded");
 
-
-
 document.addEventListener("DOMContentLoaded", function(event) {
 
   console.log("inside");
@@ -10,7 +8,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     {vendorId: 32903, productId: 2603},
     {vendorId: 32902, productId: 40239},
     {vendorId: 0043, productId: '06A8'},
-    {vendorId:'12A8', productId: '12A8'}
+    {vendorId:'12A8', productId: '12A8'},
+    {vendorId:'0A5C', productId: 5834}
   ];
 
   let button = document.getElementById('request-device');
